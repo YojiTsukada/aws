b67900d2c989fc7aabdab6c0a68bd78e08bb0c6f
YojiTsukada <>
2018-06-06 00:20:36 +0900

    make logfile to put to s3

--- rekognition.py
+++ rekognition.py
@@ -2,17 +2,20 @@ import boto3
 import sys
 import os
 
-
+# import account information.
 from account import *
-print(AWS_ACCESS_KEY_ID)
-print(AWS_SECRET_ACCESS_KEY)
 
+# setup s3 client.
+s3 = boto3.resource('s3')
 
+# setup rekognition client.
 client = boto3.client('rekognition','ap-northeast-1')
 
+# read a image.
 bucket = 'frombk'
-name = 'P4060899.JPG'
+name = 'f.jpg'
 
+# detect label.
 response = client.detect_labels(
     Image={
         'S3Object': {
@@ -22,6 +25,27 @@ response = client.detect_labels(
     }
 )
 
-print('Detecting .... Labels')
-for label in response['Labels']:
-    print(label['Name'] + " : " + str(label['Confidence']))
+# Download log 
+dest_bucket = 'detbk'
+logfile = "rekogniton.csv"
+
+try:
+    s3.Bucket(dest_bucket).download_file(logfile, '/tmp/' + logfile)
+except:
+    # make blank file
+    with open('/tmp/' + logfile,"w"):pass    
+
+# print result.
+print('Detecting .... ' + name +  ' Labels')
+
+with open('/tmp/' + logfile, "a") as file:
+    file.write(name + ', ')
+    for label in response['Labels']:
+        print(label['Name'] + " : " + str(label['Confidence']))
+        # write logfile.
+        file.write(label['Name'] + ", " + str(label['Confidence']) + ", ")
+    file.write('\n')
+
+
+# upload to S3
+s3.Bucket(dest_bucket).upload_file('/tmp/' + logfile, logfile)
